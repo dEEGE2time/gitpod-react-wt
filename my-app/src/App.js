8f9bc2e3989668f5be7b1691c83af0fcc215c206
dEEGE2time <johannesweidensjo@hotmail.com>
2023-07-21 15:39:11 +0000

    increment one and five

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,10 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import FunctionalGreetingWithProps from './components/FunctionalGreetingWithProps';
+import FunctionalGreeting from './components/FunctionalGreeting';
+import StatefulGreeting from './components/StatefulGreeting';
 
 function App() {
   return (
     <div className="App">
+      <StatefulGreeting greeting="I'm a stateful class component!" name="dEEGE" />
       <FunctionalGreetingWithProps greeting="nice to meet you!" name="dEEGE" age="99" />
     </div>
   );
