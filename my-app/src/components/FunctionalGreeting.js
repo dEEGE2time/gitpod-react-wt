e60cd1f4dec9707a3fef40425baaea77066755e4
dEEGE2time <johannesweidensjo@hotmail.com>
2023-07-21 13:47:43 +0000

    functional with props

---
+++ my-app/src/components/FunctionalGreeting.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+// function FunctionalGreeting() {
+//     return <h1>Hello from React!</h1>
+// }
+
+
+// !!!                                              !!!
+// Below is the above code converted to arrow functions.
+// !!!                                              !!!
+
+
+const FunctionalGreeting = () => <h1>Hello from dEEGE!</h1>
+
+export default FunctionalGreeting;
