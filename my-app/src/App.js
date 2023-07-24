e7134f4010c891c17ab95eddf38c608921a8fb94
dEEGE2time <johannesweidensjo@hotmail.com>
2023-07-24 19:49:06 +0000

    lifecycles methods

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,7 +1,7 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 import FunctionalGreetingWithProps from './components/FunctionalGreetingWithProps';
-import FunctionalGreeting from './components/FunctionalGreeting';
+// import FunctionalGreeting from './components/FunctionalGreeting';
 import StatefulGreeting from './components/StatefulGreeting';
 import EventsFunctional from './components/EventsFunctional';
 import EventsClass from './components/EventsClass';
@@ -10,6 +10,8 @@ import ConditionalRenderingClass from './components/ConditionalRenderingClass';
 import ConditionalRenderingFunctional from './components/ConditionalRenderingFunctional';
 import NestingComponents from './components/NestingComponents';
 import MethodsAsPropsParent from './components/MethodsAsPropsParent';
+import RenderingLists from './components/RenderingLists';
+import LifeCyclesCDM from './components/LifeCyclesCDM';
 
 function App() {
   return (
@@ -24,9 +26,13 @@ function App() {
       <ConditionalRenderingClass />
       <ConditionalRenderingFunctional connected={true}/>
       <LineBreaks lesson='Nesting Components'/>
-      <NestingComponents /> */}
+      <NestingComponents />
       <LineBreaks lesson='Methods as props'/>
       <MethodsAsPropsParent />
+      <LineBreaks lesson='Rendering Lists'/>
+      <RenderingLists /> */}
+      {/* <LineBreaks lesson='Methods Lifecycle'/> */}
+      <LifeCyclesCDM />
     </div>
   );
 }
